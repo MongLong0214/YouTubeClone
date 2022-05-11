d0cbeb535aaa252db42756ac3d437f0d30df38ee
MongLong0214 <weplay0628@gmail.com>
2022-05-11 16:01:52 +0900

    back merge

--- back/src/app.js
+++ back/src/app.js
@@ -1,5 +1,14 @@
 import express from "express";
 
+import { registerRouter } from "./routes/registerRouter";
+import { loginRouter } from "./routes/loginRouter";
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(registerRouter);
+app.use(loginRouter);
 
 export { app };
