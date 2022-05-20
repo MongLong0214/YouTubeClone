6da9d8fbe08002e3e23aa921c29c09d0a3432ba1
MongLong0214 <weplay0628@gmail.com>
2022-05-20 13:56:11 +0900

    Test

--- front/src/Header.js
+++ front/src/Header.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import { useRecoilValue } from "recoil";
-import { userInfoState } from "../../front/src/atom";
-import { Link } from "react-router-dom";
+import { useRecoilValue } from 'recoil';
+import { userInfoState } from '../../front/src/atom';
+import { Link } from 'react-router-dom';
+
+import { useNavigate } from 'react-router-dom';
 
 import {
   HeaderContainer,
   HeaderLogo,
   HeaderLeftContainer,
-} from "../src/styledCompo/HeaderStyle";
-import Logo from "../src/components/Img/Logo.png";
+  Upload,
+} from '../src/styledCompo/HeaderStyle';
+import Logo from '../src/components/Img/Logo.png';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <>
       <HeaderContainer>
         {/* <HeaderLeftContainer> */}
         <HeaderLogo src={Logo} />
+        <Upload onClick={() => navigate('/', { replace: true })}>Upload</Upload>
         {/* </HeaderLeftContainer> */}
       </HeaderContainer>
     </>
