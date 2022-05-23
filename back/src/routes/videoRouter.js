6fa07940121917c566e3eb35902da2f903da6245
MongLong0214 <weplay0628@gmail.com>
2022-05-23 16:16:48 +0900

    [Fixed] 썸네일 저장 기능 완료

--- back/src/routes/videoRouter.js
+++ back/src/routes/videoRouter.js
@@ -58,7 +58,7 @@ videoRouter.post("/video/thumbnail", async (req, res) => {
   ffmpeg(req.body.url)
     .on("filenames", function (filenames) {
       console.log("Will generate " + filenames.join(", "));
-      thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+      thumbsFilePath = "thumbnails/" + filenames[0];
     })
     .on("end", function () {
       console.log("Screenshots taken");
@@ -82,7 +82,7 @@ videoRouter.post("/video/thumbnail", async (req, res) => {
     });
 });
 
-videoRouter.post("/video/uploadVideo", loginRequired, async (req, res) => {
+videoRouter.post("/video/uploadVideo", async (req, res) => {
   //비디오 정보들을 저장
   try {
     VideoService.create(req.body);
