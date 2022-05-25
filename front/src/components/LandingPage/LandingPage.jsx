868d7d9d4593588df8edd989eeeff924621dc59d
MongLong0214 <weplay0628@gmail.com>
2022-05-25 18:07:21 +0900

    test

--- front/src/components/LandingPage/LandingPage.jsx
+++ front/src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Avatar, Col, Typography, Row } from 'antd';
-import axios from 'axios';
-import moment from 'moment';
+import React, { useEffect, useState } from "react";
+import { Card, Avatar, Col, Typography, Row } from "antd";
+import axios from "axios";
+import moment from "moment";
 
 const { Title } = Typography;
 const { Meta } = Card;
@@ -10,12 +10,12 @@ function LandingPage() {
   const [Videos, setVideos] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/video/getVideos').then((response) => {
+    axios.get("http://localhost:3001/video/getVideos").then((response) => {
       if (response.data) {
         console.log(response.data.videos);
         setVideos(response.data.videos);
       } else {
-        alert('비디오를 가져오는데 실패하였습니다.');
+        alert("비디오를 가져오는데 실패하였습니다.");
       }
     });
   }, []);
@@ -24,12 +24,14 @@ function LandingPage() {
     let minutes = Math.floor(video.duration / 60);
     let seconds = Math.floor(video.duration - minutes * 60);
 
+    console.log(video.writer);
+
     return (
       <Col lg={6} md={8} xs={24}>
-        <div style={{ position: 'relative' }}>
+        <div style={{ position: "relative" }}>
           <a href={`/video/${video._id}`}>
             <img
-              style={{ width: '100%' }}
+              style={{ width: "100%" }}
               alt="thumbnail"
               src={`http://localhost:3001/${video.thumbnail}`}
             />
@@ -38,17 +40,17 @@ function LandingPage() {
               style={{
                 bottom: 0,
                 right: 0,
-                position: 'absolute',
-                margin: '4px',
-                color: '#fff',
-                backgroundColor: 'rgba(17, 17, 17, 0.8)',
+                position: "absolute",
+                margin: "4px",
+                color: "#fff",
+                backgroundColor: "rgba(17, 17, 17, 0.8)",
                 opacity: 0.8,
-                padding: '2px 4px',
-                borderRadius: '2px',
-                letterSpacing: '0.5px',
-                fontSize: '12px',
-                fontWeight: '500',
-                lineHeight: '12px',
+                padding: "2px 4px",
+                borderRadius: "2px",
+                letterSpacing: "0.5px",
+                fontSize: "12px",
+                fontWeight: "500",
+                lineHeight: "12px",
               }}
             >
               <span>
@@ -58,17 +60,20 @@ function LandingPage() {
           </a>
         </div>
         <br />
-        <Meta avatar={<Avatar src={video.writer.image} />} title={video.title} />
+        <Meta
+          avatar={<Avatar src={video.writer.image} />}
+          title={video.title}
+        />
         <span>{video.writer.name} </span>
         <br />
-        <span style={{ marginLeft: '3rem' }}> {video.views}</span>-{' '}
-        <span> {moment(video.createdAt).format('MMM Do YY')} </span>
+        <span style={{ marginLeft: "3rem" }}> {video.views}</span>-{" "}
+        <span> {moment(video.createdAt).format("MMM Do YY")} </span>
       </Col>
     );
   });
 
   return (
-    <div style={{ width: '75%', margin: '2rem auto' }}>
+    <div style={{ width: "75%", margin: "2rem auto" }}>
       <Title level={2}> Recommended </Title>
       <hr />
 
