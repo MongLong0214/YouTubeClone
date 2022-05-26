01e4328963a5110fbf7c5cdc4c01a630f7a1405b
MongLong0214 <weplay0628@gmail.com>
2022-05-26 17:41:20 +0900

    [feature] 비디오 업로드 시 카테고리, 프라이빗 선택 박스 삭제, 기타 디자인 수정

--- front/src/components/VideoUploadPage/VideoUploadPage.jsx
+++ front/src/components/VideoUploadPage/VideoUploadPage.jsx
@@ -9,21 +9,27 @@ import Axios from "axios";
 import { useRecoilValue, useRecoilState } from "recoil";
 import { userInfoState, tokenState } from "../../atom";
 
+import Container from "@mui/material/Container";
+
+// import styled compo
+import { ValidationTextField, ColorButton } from "../../styledCompo/muiCustom";
+import { UploadTitle, UploadGlass } from "../../styledCompo/VideoUploadStyle";
+
 const { Title } = Typography;
 const { TextArea } = Input;
 
-const Private = [
-  { value: 0, label: "Private" },
-  { value: 1, label: "Public" },
-];
+// const Private = [
+//   { value: 0, label: "Private" },
+//   { value: 1, label: "Public" },
+// ];
 
-const Catogory = [
-  { value: 0, label: "Film & Animation" },
-  { value: 0, label: "Autos & Vehicles" },
-  { value: 0, label: "Music" },
-  { value: 0, label: "Pets & Animals" },
-  { value: 0, label: "Sports" },
-];
+// const Catogory = [
+//   { value: 0, label: "Film & Animation" },
+//   { value: 0, label: "Autos & Vehicles" },
+//   { value: 0, label: "Music" },
+//   { value: 0, label: "Pets & Animals" },
+//   { value: 0, label: "Sports" },
+// ];
 
 function VideoUploadPage() {
   const user = useRecoilValue(userInfoState);
@@ -36,8 +42,6 @@ function VideoUploadPage() {
 
   const [title, setTitle] = useState("");
   const [Description, setDescription] = useState("");
-  const [privacy, setPrivacy] = useState(0);
-  const [Categories, setCategories] = useState("Film & Animation");
 
   const [FilePath, setFilePath] = useState("");
   const [Duration, setDuration] = useState("");
@@ -53,14 +57,6 @@ function VideoUploadPage() {
     setDescription(event.currentTarget.value);
   };
 
-  const handleChangeOne = (event) => {
-    setPrivacy(event.currentTarget.value);
-  };
-
-  const handleChangeTwo = (event) => {
-    setCategories(event.currentTarget.value);
-  };
-
   const onSubmit = (event) => {
     event.preventDefault();
 
@@ -68,24 +64,15 @@ function VideoUploadPage() {
       return alert("Please Log in First");
     }
 
-    if (
-      title === "" ||
-      Description === "" ||
-      Categories === "" ||
-      FilePath === "" ||
-      Duration === "" ||
-      Thumbnail === ""
-    ) {
-      return alert("Please first fill all the fields");
+    if (title === "" || Description === "" || Thumbnail === "") {
+      return message.error("모든 항목을 채워주세요");
     }
 
     const variables = {
       writer: user._id,
       title: title,
       description: Description,
-      privacy: privacy,
       filePath: FilePath,
-      category: Categories,
       duration: Duration,
       thumbnail: Thumbnail,
     };
@@ -105,25 +92,6 @@ function VideoUploadPage() {
       }
     );
   };
-
-  //   const onDrop = async (files) => {
-  //     let formData = new FormData();
-  //     const config = { header: { 'content-type': 'multipart/form-data' } };
-  //     formData.append('file', files[0]);
-
-  //     console.log(files);
-
-  //     try {
-  //       const res = await Api.post('video/upload', formData, config);
-  //       if (res.data.success) {
-  //         console.log('결과', res.data);
-  //       } else {
-  //         alert('Failed to upload video');
-  //       }
-  //     } catch (err) {
-  //       console.log('failed');
-  //     }
-  //   };
 
   const onDrop = (files) => {
     let formData = new FormData();
@@ -164,131 +132,97 @@ function VideoUploadPage() {
   console.log("Duration", Duration);
   console.log("Thumbnail", Thumbnail);
 
-  //   const onDrop = (files) => {
-  //     let formData = new FormData();
-  //     const config = {
-  //       header: { 'content-type': 'multipart/form-data' },
-  //     };
-  //     console.log(files);
-  //     formData.append('file', files[0]);
-
-  //     Axios.post('http://localhost:3001/video/upload', formData, config).then((response) => {
-  //       if (response.data.success) {
-  //         let variable = {
-  //           filePath: response.data.filePath,
-  //           fileName: response.data.fileName,
-  //         };
-  //         setFilePath(response.data.filePath);
-
-  //         //gerenate thumbnail with this filepath !
-
-  //         Axios.post('http://localhost:3001/video/thumbnail', variable).then((response) => {
-  //           if (response.data.success) {
-  //             setDuration(response.data.fileDuration);
-  //             setThumbnail(response.data.thumbsFilePath);
-  //           } else {
-  //             alert('Failed to make the thumbnails');
-  //           }
-  //         });
-  //       } else {
-  //         alert('failed to save the video in server');
-  //       }
-  //     });
-  //   };
-
-  //     Api.post('video/upload', formData, config).then((response) => {
-  //       if (response.data.success) {
-  //         let variable = {
-  //           filePath: response.data.filePath,
-  //           fileName: response.data.fileName,
-  //         };
-  //         setFilePath(response.data.filePath);
-
-  //         Api.post('video/thumbnail', variable).then((response) => {
-  //           console.log('thumbnail====>', response);
-  //           if (response.data.success) {
-  //             setDuration(response.data.fileDuration);
-  //             setThumbnail(response.data.thumbsFilePath);
-  //           } else {
-  //             alert('Failed to make the thumbnails');
-  //           }
-  //         });
-  //       } else {
-  //         alert('failed to save the video in server');
-  //       }
-  //     });
-  //   };
-
   return (
-    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
-      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
-        <Title level={2}> Upload Video</Title>
-      </div>
+    <>
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "120px",
+        }}
+      >
+        <UploadGlass>
+          <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+            <UploadTitle style={{ textAlign: "center" }}>
+              VideoUpload
+            </UploadTitle>
 
-      <Form onSubmit={onSubmit}>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
-            {({ getRootProps, getInputProps }) => (
+            <Form onSubmit={onSubmit}>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
+                  {({ getRootProps, getInputProps }) => (
+                    <div
+                      style={{
+                        width: "300px",
+                        height: "240px",
+                        border: "1px solid lightgray",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                      {...getRootProps()}
+                    >
+                      <input {...getInputProps()} />
+                      <PlusOutlined style={{ fontSize: "3rem" }} />
+                    </div>
+                  )}
+                </Dropzone>
+
+                {Thumbnail && (
+                  <div>
+                    <img
+                      src={`http://localhost:3001/${Thumbnail}`}
+                      alt="Thumbnail"
+                    />
+                  </div>
+                )}
+              </div>
+
+              <br />
+              <ValidationTextField
+                style={{ marginBottom: 20 }}
+                sm={8}
+                id="outlined-required"
+                label="제목"
+                value={title}
+                onChange={handleChangeTitle}
+              />
+              <ValidationTextField
+                style={{ marginBottom: 30 }}
+                sm={8}
+                id="outlined-required"
+                label="설명"
+                value={Description}
+                onChange={handleChangeDecsription}
+              />
+
               <div
                 style={{
-                  width: "300px",
-                  height: "240px",
-                  border: "1px solid lightgray",
                   display: "flex",
-                  alignItems: "center",
                   justifyContent: "center",
                 }}
-                {...getRootProps()}
               >
-                <input {...getInputProps()} />
-                <PlusOutlined style={{ fontSize: "3rem" }} />
+                <ColorButton
+                  variant="outlined"
+                  type="submit"
+                  onClick={onSubmit}
+                >
+                  저장
+                </ColorButton>
+                <ColorButton
+                  variant="outlined"
+                  type="submit"
+                  onClick={() => navigate("/")}
+                  style={{ marginLeft: "10px" }}
+                >
+                  취소
+                </ColorButton>
               </div>
-            )}
-          </Dropzone>
-
-          {Thumbnail && (
-            <div>
-              <img src={`http://localhost:3001/${Thumbnail}`} alt="Thumbnail" />
-            </div>
-          )}
-        </div>
-
-        <br />
-        <br />
-        <label>Title</label>
-        <Input onChange={handleChangeTitle} value={title} />
-        <br />
-        <br />
-        <label>Description</label>
-        <TextArea onChange={handleChangeDecsription} value={Description} />
-        <br />
-        <br />
-
-        <select onChange={handleChangeOne}>
-          {Private.map((item, index) => (
-            <option key={index} value={item.value}>
-              {item.label}
-            </option>
-          ))}
-        </select>
-        <br />
-        <br />
-
-        <select onChange={handleChangeTwo}>
-          {Catogory.map((item, index) => (
-            <option key={index} value={item.label}>
-              {item.label}
-            </option>
-          ))}
-        </select>
-        <br />
-        <br />
-
-        <Button type="primary" size="large" onClick={onSubmit}>
-          Submit
-        </Button>
-      </Form>
-    </div>
+            </Form>
+          </div>
+        </UploadGlass>
+      </Container>
+    </>
   );
 }
 
